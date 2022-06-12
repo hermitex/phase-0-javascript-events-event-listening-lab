273b3dc109359588d94856bf6a553a3f6d7517bc
hermitex <mercytibs@yahoo.com>
2022-06-12 16:51:43 +0300

    commit tree 9506c1068352ce3605665d08d180915cfef5b72f

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 function addingEventListener() {
+    const input = document.querySelector('#input');
+    input.addEventListener('input', ()=>console.log('input has started'))
 }
+
+addingEventListener();
